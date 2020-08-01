f8a59db3df873c420762a344be8a63b37a13952c
lovelife <lovelife@kaist.ac.kr>
2020-08-02 02:41:12 +0900

    Initial structure setting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Initial Post",
+        "excerpt":"This is the first post!  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/initial-post/",
+        "teaser": null
+      }]
