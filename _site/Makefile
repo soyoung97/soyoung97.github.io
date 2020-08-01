189f1902096499da436b13b13169955fc72883b3
lovelife <lovelife@kaist.ac.kr>
2020-08-02 02:56:09 +0900

    Create profile branch

--- _site/Makefile
+++
@@ -1,3 +0,0 @@
-Personal website for soyoung.
-Created by using minimal mistakes jekyll theme.
-#for dev, run bundle exec jekyll serve
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Initial Post",
-        "excerpt":"This is the first post!  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/initial-post/",
-        "teaser": null
-      }]
