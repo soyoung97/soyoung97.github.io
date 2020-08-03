dd1c0acc9daf8550f258549c7554ae3dcbb3b734
lovelife <lovelife@kaist.ac.kr>
2020-08-04 00:49:48 +0900

    Add ELECTRA

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/initial-post/",
         "teaser": null
+      },{
+        "title": "Electra",
+        "excerpt":"ELECTRA: Pre-training Text Encoders as Discriminators Rather Than Generators  (openreview link)   첫 글은 연구실 논문리딩그룹에서 내가 발표했던 슬라이드를 활용하기로 하였다. 시간이 있을 때 설명을 더 추가할 예정!          ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/electra/",
+        "teaser": null
       }]
